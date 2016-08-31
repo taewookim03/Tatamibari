41a95f5ca8a880d59beead85ba614fd6f5070e3d
Taewoo Kim <taewookim03@gmail.com>
2016-08-30 20:11:20 -0500

    Base libGDX project files

---
+++ desktop/src/com/tatamibari/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tatamibari.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tatamibari.TatamibariGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TatamibariGame(), config);
+	}
+}
