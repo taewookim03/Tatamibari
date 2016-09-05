fa7ecdf7c94181bed10747405b4496a031728550
Taewoo Kim <taewookim03@gmail.com>
2016-09-04 22:34:41 -0500

    Added blend function so alpha works

--- core/src/com/tatamibari/TatamibariGame.java
+++ core/src/com/tatamibari/TatamibariGame.java
@@ -31,8 +31,11 @@ public class TatamibariGame extends Game {
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1,1,1,1);
-		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling?GL20.GL_COVERAGE_BUFFER_BIT_NV:0));
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.graphics.getGL20().glEnable(GL20.GL_BLEND);
+        Gdx.graphics.getGL20().glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
+		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling?GL20.GL_COVERAGE_BUFFER_BIT_NV:0));
 		world.act();
 		world.draw();
 	}
